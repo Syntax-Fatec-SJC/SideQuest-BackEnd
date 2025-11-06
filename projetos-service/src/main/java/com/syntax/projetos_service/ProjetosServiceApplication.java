6c3ab1a8d3d80f050d6468822ce4acec659be109
LucasAraujo1016 <lucas.allbuquerque1234@gmail.com>
2025-11-05 21:01:44 -0300

    Feat: Starting a microservice structure

---
+++ projetos-service/src/main/java/com/syntax/projetos_service/ProjetosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syntax.projetos_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosServiceApplication.class, args);
+	}
+
+}
